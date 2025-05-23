e42ad74742836e6206a9e5a5b0e599b20ce035b7
ram lande <manoijkumar.lande@gmail.com>
2025-05-23 11:38:29 +0530

    commit allpages

---
+++ sb/Finalkeyword/src/package1/Parent.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Parent {
+	
+	
+	
+	
+   final     public String a="ghhj";
+	
+	final    public  int b=342;
+	
+	final    public void method()
+	        {
+		
+		
+		System.out.println("this is an final class method");
+		
+	    }
+	
+	
+
+}
---
+++ sb/Finalkeyword/src/package1/Test.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class Test extends Parent {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Parent p= new Parent();
+		
+		   p.method();
+		   
+		   int d=p.b;
+		   
+		   String c=p.a;
+		   
+		   System.out.println(p.a);
+		
+		   System.out.println(p.b);
+		
+	}
+	
+
+}
